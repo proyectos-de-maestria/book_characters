b8142c66b3a8d160f03898a98920943b56126744
m1gu3 <94m1gu3@gmail.com>
2019-06-26 17:00:17 -0400

    tested a conversational graph in pride and prejudice extract

---
+++ base_code/graph.py
@@ -0,0 +1,43 @@
+import networkx as nx
+from networkx.readwrite import gml
+import matplotlib.pyplot as plt
+from collections import Counter
+
+
+def add_nodes(graph, nodes):
+    nodes = Counter(nodes)
+    
+    for name, count in nodes.items():
+        node_count = (graph.node[name]['count'] if name in graph else 0) + count
+        graph.add_node(name, count=node_count)
+
+    names = nodes.keys()
+    edges_kn = [(x, y) for x in names for y in names if x != y]
+
+    graph.add_edges_from(edges_kn, color='red')
+
+
+def paint_graph(graph, name):
+    pos = nx.circular_layout(graph)
+    # pos = nx.spring_layout(graph, k=0.70,iterations=20)
+    # nodes
+    nx.draw_networkx_nodes(graph, pos, node_size=150, alpha=0.8)
+    # edges
+    nx.draw_networkx_edges(graph, pos)
+
+    labels = {}
+    for node in graph.node.keys():
+        labels[node] = node
+    nx.draw_networkx_labels(graph,pos,labels,font_size=8, font_color='red', font_weight='bold')    
+
+    plt.axis('off')
+    plt.savefig(name + ".png")        # save as png
+    plt.show()
+
+
+def save_graph(graph, name):
+    gml.write_gml(graph, name + ".gml")
+
+
+def load_graph(name):
+    return gml.read_gml(name + ".gml")
--- conversational_net/quoted_speech.py
+++ conversational_net/quoted_speech.py
@@ -1,15 +1,29 @@
 from conversational_net.quotes_utils import split_in_full_conversation
+from base_code.preprocessing import names_in_text
+from base_code.graph import *
 
 
-def get_conversational_graph(text):
+def get_conversational_graph(text, graph_name):
     full_talks = split_in_full_conversation(text)
     for i in full_talks:
         print("----------")
         print(i)
+    conv_names = []
+    for talk in full_talks:
+        conv_names.append(names_in_text(talk))
+
+    graph = nx.Graph()
+    for names in conv_names:
+        add_nodes(graph, names)
+
+    save_graph(graph, graph_name)
+    paint_graph(graph, graph_name)
+    return graph
 
 
 if __name__ == '__main__':
-    t = open("../books/pride and prejudice extract.txt", encoding="utf8")
+    book_name = "pride and prejudice extract"
+    t = open("../books/" + book_name + ".txt", encoding="utf8")
     rd = t.read()
-    # print(rd)
-    get_conversational_graph(rd)
+
+    get_conversational_graph(rd, "./graphs/conv_" + book_name)
